56e5adc8f6a3e1bb34dff18a4b00cda677c0c58f
Gabriel Ramalho <gabrielhramalho009@gmail.com>
2026-02-07 17:07:19 -0300

    refactor: abstracts classes Curso and Mentoria using Conteudo class

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,33 +1,16 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-	private String titulo;
-	private String descricao;
+public class Curso extends Conteudo {
 	private int cargaHoraria;
 
-	public Curso() {
-	}
-
 	public Curso(String titulo, String descricao, int cargaHoraria) {
-		this.titulo = titulo;
-		this.descricao = descricao;
+		super(titulo, descricao);
 		this.cargaHoraria = cargaHoraria;
 	}
 
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+	@Override
+	public double calcularXp() {
+		return XP_PADRAO + 20d;
 	}
 
 	public int getCargaHoraria() {
@@ -41,8 +24,8 @@ public class Curso {
 	@Override
 	public String toString() {
 		return "Curso{" +
-				       "titulo='" + titulo + '\'' +
-				       ", descricao='" + descricao + '\'' +
+				       "titulo='" + getTitulo() + '\'' +
+				       ", descricao='" + getDescricao() + '\'' +
 				       ", cargaHoraria=" + cargaHoraria +
 				       '}';
 	}
